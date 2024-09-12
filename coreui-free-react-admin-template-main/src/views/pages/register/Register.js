2a9f5fba2f0367701493a8602076dd6d59ee2ae5
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-09-12 20:27:05 +0530

    registration completed

--- coreui-free-react-admin-template-main/src/views/pages/register/Register.js
+++ coreui-free-react-admin-template-main/src/views/pages/register/Register.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   CButton,
   CCard,
@@ -13,8 +13,47 @@ import {
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
+const host = 'http://localhost:4000'
+    const [credential, setCredentials] = useState({
+      name: '',
+      email: '',
+      password: '',
+      cpassword: '',
+    })
+    let navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+      console.log("register of user triggered");
+      
+      e.preventDefault()
+      const { name, email, password } = credential
+
+      const response = await fetch(`${host}/api/auth/createuser`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          password: password,
+        }),
+      })
+      const json = await response.json()
+
+      if (json.success) {
+        localStorage.setItem('token', json.token)
+        navigate('/login')
+      }
+    }
+
+    const onChange = (e) => {
+      setCredentials({ ...credential, [e.target.name]: e.target.value })
+    }
+  
   return (
     <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
@@ -22,18 +61,30 @@ const Register = () => {
           <CCol md={9} lg={7} xl={6}>
             <CCard className="mx-4">
               <CCardBody className="p-4">
-                <CForm>
+                <CForm onSubmit={handleSubmit}>
                   <h1>Register</h1>
                   <p className="text-body-secondary">Create your account</p>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>
                       <CIcon icon={cilUser} />
                     </CInputGroupText>
-                    <CFormInput placeholder="Username" autoComplete="username" />
+                    <CFormInput
+                      placeholder="Username"
+                      autoComplete="username"
+                      id="name"
+                      name="name"
+                      onChange={onChange}
+                    />
                   </CInputGroup>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>@</CInputGroupText>
-                    <CFormInput placeholder="Email" autoComplete="email" />
+                    <CFormInput
+                      placeholder="Email"
+                      autoComplete="email"
+                      id="email"
+                      name="email"
+                      onChange={onChange}
+                    />
                   </CInputGroup>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>
@@ -43,6 +94,9 @@ const Register = () => {
                       type="password"
                       placeholder="Password"
                       autoComplete="new-password"
+                      id="password"
+                      name="password"
+                      onChange={onChange}
                     />
                   </CInputGroup>
                   <CInputGroup className="mb-4">
@@ -56,7 +110,9 @@ const Register = () => {
                     />
                   </CInputGroup>
                   <div className="d-grid">
-                    <CButton color="success">Create Account</CButton>
+                    <CButton type="submit" color="success">
+                      Create Account
+                    </CButton>
                   </div>
                 </CForm>
               </CCardBody>
